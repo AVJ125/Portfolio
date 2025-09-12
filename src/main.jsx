07ab7d97c58f73a459932d382e5edf5ccfd43f1a
Aniket Joshi <anijoshi@qualys.com>
2025-09-13 02:41:59 +0530

    Dark mode analytics added

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import { createBrowserRouter,RouterProvider } from 'react-router-dom'
 import About from './assets/Endpoints/About.jsx'
 import Experience from './assets/Endpoints/Experience.jsx'
 import Projects from './assets/Endpoints/Projects.jsx'
+import { Analytics } from "@vercel/analytics/react"
+
 import Rootlayout from './assets/Endpoints/Rootlayout.jsx'
 import Navigations from './assets/Endpoints/navigation.jsx'
 import Orb from './assets/components/Orb.jsx';
@@ -28,5 +30,6 @@ createRoot(document.getElementById('root')).render(
     
     <SplashCursor/>
 
+    <Analytics/>
   </StrictMode>,
 )
